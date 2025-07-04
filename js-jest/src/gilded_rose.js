817aa23a832d6d643563d6296c4ff634c4f74c78
Daniel Guryev <Daniel.Guryev@gmail.com>
2025-07-04 12:09:44 +0100

    move quality calculation into calculateQualityChange

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -10,6 +10,9 @@ const sulfuras = 'Sulfuras, Hand of Ragnaros';
 const brie = 'Aged Brie';
 const backstage = 'Backstage passes to a TAFKAL80ETC concert';
 
+const qualUpperLimit = 50;
+const qualLowerLimit = 0;
+
 class Shop {
   constructor(items=[]){
     this.items = items;
@@ -19,46 +22,43 @@ class Shop {
 
       if (item.name == sulfuras) continue;
 
-
-      let qualityChange;
-
-      if (item.name == backstage) {
-        if (item.sellIn < 6)
-          qualityChange = 3
-        else if (item.sellIn < 11)
-          qualityChange = 2
-        else qualityChange = 1
-      }
-      else if (item.name == brie)
-        qualityChange = 1 
-      else
-        qualityChange = -1
-
-
-      item.sellIn --;
-
-      if (item.sellIn < 0) {
-        if (item.name == backstage) {
-          qualityChange=-item.quality
-          }
-        else {
-          qualityChange*=2
-        }
-      }
-      
-      this.adjustQuality(item, qualityChange)
+      let qualityChange = this.calculateQualityChange(item);
+  
+      item.sellIn--;
+      item.quality += qualityChange;
 
     }
 
     return this.items;
   }
 
-  adjustQuality(item, amount) {
-    if (item.quality >= 50 || item.quality <= 0) {
-      return;
-    } 
+  calculateQualityChange(item){
+    let qualityChange;
 
-    item.quality = item.quality + amount;
+    if (item.name == backstage) {
+      if (item.sellIn < 6)
+        qualityChange = 3
+      else if (item.sellIn < 11)
+        qualityChange = 2
+      else qualityChange = 1
+    }
+    else if (item.name == brie)
+      qualityChange = 1 
+    else
+      qualityChange = -1
+
+    if (item.sellIn <= 0) {
+      if (item.name == backstage) {
+        qualityChange=-item.quality
+      }
+      else {
+        qualityChange*=2
+      }
+    }
+
+    if (item.quality + qualityChange > qualUpperLimit) return qualUpperLimit - item.quality;
+    if (item.quality + qualityChange < qualLowerLimit) return qualLowerLimit - item.quality;
+    return qualityChange
   }
 
 }
